206c27b3b10a823781d7b8a8bbfbc447631c01be
Marcelo Félix <marcelofelix.af@gmail.com>
2017-05-24 10:31:48 -0300

    Após usuário selecionar o supermercado

---
+++ insere_supermercado.php
@@ -0,0 +1,26 @@
+<?php
+  header("Content-Type: text/html; charset=UTF-8",true);
+
+  $conexao = mysqli_connect("localhost", "root", "2ehpmxnd","usearch");
+
+  $nome = $_GET['nome'];
+  $numero = $_GET['numero'];
+  $rua = $_GET['rua'];
+  $bairro = $_GET['bairro'];
+  $cidade = $_GET['cidade'];
+  $estado = $_GET['estado'];
+  $pais = $_GET['pais'];
+  $lat = $_GET['lat'];
+  $lng = $_GET['lng'];
+
+  $check = "INSERT INTO supermercado (nome, numero, rua, bairro, cidade, estado, pais, latitude, longitude) VALUES ('$nome', '$numero', '$rua', '$bairro', '$cidade', '$estado', '$pais', '$lat', '$lng' )";
+
+  if (mysqli_query($conexao, $check)) {
+    $result = array('success' => true);
+    echo json_encode($result);
+  } else {
+    $result = array('success' => false);
+    echo json_encode($result);
+  }
+mysqli_close($conexao);
+?>
